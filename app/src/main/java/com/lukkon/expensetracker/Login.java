3f89b7fcf6be59b2c138857fb4b672c8275fa393
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-11-28 14:02:20 +0100

    Changed ids of login view, implemented login button click

--- app/src/main/java/com/lukkon/expensetracker/Login.java
+++ app/src/main/java/com/lukkon/expensetracker/Login.java
@@ -3,6 +3,7 @@ package com.lukkon.expensetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    protected void onLoginClick(View view){
+        setContentView(R.layout.activity_main);
     }
 }
