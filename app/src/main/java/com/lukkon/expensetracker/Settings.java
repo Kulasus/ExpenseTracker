eb83e6c111d9d821eb3d068bd3a7ebaafe8e5db6
Lukáš Kondiolka <48946785+Kulasus@users.noreply.github.com>
2020-12-12 19:45:35 +0100

    Added theme changing to setting activity

--- app/src/main/java/com/lukkon/expensetracker/Settings.java
+++ app/src/main/java/com/lukkon/expensetracker/Settings.java
@@ -1,38 +1,60 @@
 package com.lukkon.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
 
+    ConstraintLayout settingsLayout;
+    TextView settingsSoundTextView;
     RadioGroup radios;
     RadioButton soundOn;
     RadioButton soundOff;
     SharedPreferences prefs;
     boolean sound;
+    boolean lightTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        settingsLayout = findViewById(R.id.settingsLayout);
+        settingsSoundTextView = findViewById(R.id.settingsSoundTextView);
         radios = findViewById(R.id.settingsRadioGroup);
         soundOn = findViewById(R.id.soundOnRadioButtonSettings);
         soundOff = findViewById(R.id.soundOffRadioButtonSettings);
 
         prefs = this.getSharedPreferences("com.lukkon.expensetracker", Context.MODE_PRIVATE);
         sound = prefs.getBoolean("sound",true);
+        lightTheme = prefs.getBoolean("lightTheme",true);
 
         if(sound){
             soundOn.setChecked(true);
         }
         else{
             soundOff.setChecked(true);
+        }
+
+        if(lightTheme){
+            settingsSoundTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorBlack));
+            soundOff.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorBlack));
+            soundOn.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorBlack));
+            settingsLayout.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorWhite));
+        }
+        else{
+            settingsSoundTextView.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorWhite));
+            soundOff.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorWhite));
+            soundOn.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorWhite));
+            settingsLayout.setBackgroundColor(ContextCompat.getColor(getApplicationContext(),R.color.colorBlack));
         }
 
         radios.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
